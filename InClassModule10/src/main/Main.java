827d1ac2f24abe37519ab54a0753f8f4b29b4276
Trey Hollins <trey.hollins@gmail.com>
2020-03-28 17:34:38 -0400

    First Commit

---
+++ InClassModule10/src/main/Main.java
@@ -0,0 +1,14 @@
+//Module 10 in class work
+package main;
+
+import java.util.Hashtable;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//		<K,V>
+		Hashtable<String, String> myTable = new Hashtable<String, String>();
+
+	}
+
+}
